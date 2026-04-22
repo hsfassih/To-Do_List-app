a12365234ce63ad26613608b3f284250d313628f
hs-fassih <hsfassih.28@gmail.com>
2026-04-22 22:01:17 +0500

    Configured remote S3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "hsfassih-todo-app"
+    key            = "todo-app/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
@@ -22,5 +30,21 @@ module "app_bucket" {
 
   tags = {
     Project = "todo-app"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Project   = "todo-app"
+    ManagedBy = "terraform"
   }
 }
