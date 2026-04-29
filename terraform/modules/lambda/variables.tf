a3eb2f6af195541d40dee6d31858cc0040227e60
hs-fassih <hsfassih.28@gmail.com>
2026-04-29 23:49:02 +0500

    corrected syntax of lambda modules variables

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -1,7 +1,32 @@
-variable "function_name"    { type = string }
-variable "s3_bucket"        { type = string }
-variable "s3_key"           { type = string; default = "lambda/function.zip" }
-variable "source_code_hash" { type = string; default = "" }
-variable "environment"      { type = string; default = "dev" }
-variable "runtime"          { type = string; default = "python3.12" }
-variable "handler"          { type = string; default = "function1.handler" }
+variable "function_name" {
+    type = string
+}
+
+variable "s3_bucket" {
+    type = string
+}
+
+variable "s3_key" { 
+    type = string
+    default = "lambda/function.zip" 
+}
+
+variable "source_code_hash" {
+    type = string
+    default = "" 
+}
+
+variable "environment" {
+    type = string
+    default = "dev"
+    }
+
+variable "runtime" {
+    type = string
+    default = "python3.12"
+}
+
+variable "handler" {
+    type = string
+    default = "function1.handler"
+}
