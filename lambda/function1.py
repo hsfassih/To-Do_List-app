f08c8a19a5986c714565392bb9ca8c73c28a5a6b
hs-fassih <hsfassih.28@gmail.com>
2026-04-30 00:13:22 +0500

    added inline policies for the role

--- lambda/function1.py
+++ lambda/function1.py
@@ -1,6 +1,6 @@
 import json
 
 
-# testing workflow fristly with a simple hello function
+# testing the workflow fristly with a simple hello function
 def handler(event, context):
     return {"statusCode": 200, "body": json.dumps({"message": "Hello from Lambda!"})}
